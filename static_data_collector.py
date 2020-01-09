980c05f98567613ea5b2693768f86b850962b224
KarenImmanuel <karenimmanuel@karunya.edu.in>
2020-01-09 09:43:10 +0530

    Update static_data_collector.py

--- static_data_collector.py
+++ static_data_collector.py
@@ -1,5 +1,21 @@
 import requests, bs4
-res = requests.get('http://nostarch.com')
+#! python3
+# Opens several Google search results.
+
+import requests, sys, webbrowser, bs4
+
+print('Searching...') # display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text)
-print(type(noStarchSoup))
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
